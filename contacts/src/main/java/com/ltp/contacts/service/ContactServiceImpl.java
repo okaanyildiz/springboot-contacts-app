4d8c9ea6c8b5448cd1520066c3a77627bf4d5c32
Ozi <oguzkaanyildiz@mac-ozi.local>
2022-12-31 10:23:14 +0100

    Add put and delete requests

--- contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
+++ contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ltp.contacts.service;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,21 @@ public class ContactServiceImpl implements ContactService {
     @Override
     public void saveContact(Contact contact) {
         contactRepository.saveContact(contact);
+    }
+
+    @Override
+    public void updateContact(String id, Contact contact) {
+        contactRepository.updateContact(findIndexById(id), contact);
+    }
+
+    @Override
+    public void deleteContact(String id) {
+        contactRepository.deleteContact(findIndexById(id));
+    }
+
+    @Override
+    public List<Contact> getContacts() {
+        return contactRepository.getContacts();
     }
 
     private int findIndexById(String id) {

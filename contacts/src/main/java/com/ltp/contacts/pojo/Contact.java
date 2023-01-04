2ad145839f58e740b41cb29417ba9bd91a20040e
Ozi <oguzkaanyildiz@mac-ozi.local>
2023-01-04 05:42:36 +0100

    Add Basic Auth

--- contacts/src/main/java/com/ltp/contacts/pojo/Contact.java
+++ contacts/src/main/java/com/ltp/contacts/pojo/Contact.java
@@ -2,15 +2,32 @@ package com.ltp.contacts.pojo;
 
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+
 public class Contact {
 
     private String id;
+
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
+    @NotBlank(message = "Phone number cannot be blank")
     private String phoneNumber;
 
-    public Contact() {
-        this.id = UUID.randomUUID().toString();
+    public Contact(String id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
     }
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Contact(){
+        this.id = UUID.randomUUID().toString();
+    } 
 
     public String getName() {
         return this.name;
--- contacts/src/main/java/com/ltp/contacts/repository/ContactRepository.java
+++ contacts/src/main/java/com/ltp/contacts/repository/ContactRepository.java
@@ -1,7 +1,6 @@
 package com.ltp.contacts.repository;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -10,7 +9,7 @@ import com.ltp.contacts.pojo.Contact;
 
 @Repository
 public class ContactRepository {
-
+    
     private List<Contact> contacts = new ArrayList<>();
 
     public List<Contact> getContacts() {
@@ -25,12 +24,9 @@ public class ContactRepository {
         contacts.add(contact);
     }
 
-    public void updateContact(int index, Contact contact) {
-        contacts.set(index, contact);
-    }
-
     public void deleteContact(int index) {
         contacts.remove(index);
     }
+
 
 }
